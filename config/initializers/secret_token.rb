47c38ae17851c22ac64b1504670c9aee4752233a
Win Brisbin <win@winbrisbin.com>
2013-03-29 10:40:56 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '35085638d3282b0f778fd2f387f72fdd757af8be637140d9e75148dc025b960eaaa2af199f48054e0c866fb038e9f81fbbee39c53283d879fa5a76b40dcafb9f'
